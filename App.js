c4f52c2de36b140d5b3fc3d61c27da48f69f670b
Shivam Mishra <63857928+ShivamYash742@users.noreply.github.com>
2024-11-16 21:04:03 +0530

    15-11-2024 / Friday

--- App.js
+++ App.js
@@ -1,29 +1,16 @@
 const fs = require("fs");
-const readline = require("readline");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+let a = 20;
+let b = 30;
 
-function addNumbers(a, b) {
+const sum = (a, b) => {
   return a + b;
 }
 
-rl.question("Enter the first number: ", (firstInput) => {
-  rl.question("Enter the second number: ", (secondInput) => {
-    const num1 = parseFloat(firstInput);
-    const num2 = parseFloat(secondInput);
-
-    const sum = addNumbers(num1, num2);
-
-    fs.writeFile("./output.txt", `The sum of ${num1} and ${num2} is ${sum}`, (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("File Written Successfully");
-      }
-      rl.close();
-    });
-  });
+fs.writeFile("./output.txt", `The sum of ${a} and ${b} is ${sum(a, b)}`, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("File Written Successfully");
+  }
 });
